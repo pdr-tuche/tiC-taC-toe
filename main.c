2d2de7bf8315d0d271334b825479818093caf7c9
Pedro Neves <contato.nevespedro@gmail.com>
2022-11-17 15:52:28 -0300

    created main

---
+++ main.c
@@ -0,0 +1,17 @@
+/**
+ * @file main.c
+ * @author Pedro Neves (github.com/pdr-tuche)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-17
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdio.h>
+
+int main(){
+
+    return 0;
+}
