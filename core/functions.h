5b27d017b95b53bb2a929e3b30f6fc3dfb1df0ee
Pedro Neves <contato.nevespedro@gmail.com>
2022-11-18 15:20:55 -0300

    tictactoe done

--- core/functions.h
+++ core/functions.h
@@ -38,26 +38,61 @@ void pegarPonto (int *a, int *b){
     scanf("%d", b);
 }
 
-void mudarMatriz(int *a, int *b, char **m){
+void mudarMatriz(int *a, int *b, char **m, int jogador){
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
             if(i == *a && j == *b){
-                m[i][j] = 'X';
+                if(jogador == 1){
+                    m[i][j] = 'X';
+                }else{
+                     m[i][j] = 'O';
+                }
             }
         }
         
     }
     
 }
+
 int verificarPonto (int *a, int *b, char **mat){
-    int passouTeste =0;
+    int quantidadeDeTestes =2;
     if(*a <= 2 && *a >=0 && *b <= 2 && *b >=0){
-        passouTeste++;
+        quantidadeDeTestes--;
         if(mat[*a][*b] == ' '){
-            passouTeste++;
+            quantidadeDeTestes--;
         }
     }
-    return passouTeste;
+    return quantidadeDeTestes;
+}
+
+int verificarVencedor(char **m){
+    //linhas
+    if(m[0][0] == 'X' && m[0][1] == 'X' && m[0][2] == 'X'){return 1;}
+    if(m[1][0] == 'X' && m[1][1] == 'X' && m[1][2] == 'X'){return 1;}
+    if(m[2][0] == 'X' && m[2][1] == 'X' && m[2][2] == 'X'){return 1;}
+
+    if(m[0][0] == 'O' && m[0][1] == 'O' && m[0][2] == 'O'){return 2;}
+    if(m[1][0] == 'O' && m[1][1] == 'O' && m[1][2] == 'O'){return 2;}
+    if(m[2][0] == 'O' && m[2][1] == 'O' && m[2][2] == 'O'){return 2;}
+
+//colunas
+
+    if(m[0][0] == 'X' && m[1][0] == 'X' && m[2][0] == 'X'){return 1;}
+    if(m[0][1] == 'X' && m[1][1] == 'X' && m[2][1] == 'X'){return 1;}
+    if(m[0][2] == 'X' && m[1][2] == 'X' && m[2][2] == 'X'){return 1;}
+
+    if(m[0][0] == 'O' && m[1][0] == 'O' && m[2][0] == 'O'){return 2;}
+    if(m[0][1] == 'O' && m[1][1] == 'O' && m[2][1] == 'O'){return 2;}
+    if(m[0][2] == 'O' && m[1][2] == 'O' && m[2][2] == 'O'){return 2;}
+
+    //diagonais
+    if(m[0][0] == 'X' && m[1][1] == 'X' && m[2][2] == 'X'){return 1;}
+    if(m[0][2] == 'X' && m[1][1] == 'X' && m[2][0] == 'X'){return 1;}
+    
+    if(m[0][0] == 'O' && m[1][1] == 'O' && m[2][2] == 'O'){return 2;}
+    if(m[0][2] == 'O' && m[1][1] == 'O' && m[2][0] == 'O'){return 2;}
+    
+    return 0;
 }
--- core/main.c
+++ core/main.c
@@ -10,45 +10,62 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "functions.h"
 
 int main(){
 
-    char matriz [3][3];
-    char **mat;
+    char **matriz;
     int vezJogador = 1;
-    int flag;
+    int flagDeErros;
+    int vencedor =0;
     int vzsJogadas = 0;
     int linha, coluna;
     int *row, *col;
 
-    mat = malloc(3 * sizeof(char*));
+    matriz = malloc(3 * sizeof(char*));
     for (int i = 0; i < 3; i++)
     {
-        mat[i] = malloc(3 * sizeof(char));
+        matriz[i] = malloc(3 * sizeof(char));
     }
     
-    preencherMatriz(mat);
-    mostrarMatriz(mat);
+    preencherMatriz(matriz);
+    mostrarMatriz(matriz);
 
     row = &linha;
     col = &coluna;
 
     do{
-        pegarPonto(row, col);
-        
-        flag = verificarPonto(row, col, mat);
-        if(flag < 2){
-            printf("coordenada invalida, digite novamente \n");
-        }
-    }while(flag < 2);
+        printf("\nvez do jogador %d\n", vezJogador);
+        do{
+            pegarPonto(row, col);
+            
+            flagDeErros = verificarPonto(row, col, matriz);
+            if(flagDeErros > 0){
+                printf("coordenada invalida, digite novamente \n");
+            }
+        }while(flagDeErros != 0);
 
-    mudarMatriz(row, col, mat);
-    mostrarMatriz(mat);
-    
-    
-    
-    
+        mudarMatriz(row, col, matriz, vezJogador);
+        system("cls");
+        mostrarMatriz(matriz);
+        if(vezJogador == 1){
+            vezJogador = 2;
+        }else{
+            vezJogador = 1;
+        }
+
+        vencedor = verificarVencedor(matriz);
+        if(vencedor != 0){
+            printf("\n O JOGADOR %d E O VENCEDOR !!! \n", vencedor);
+            break;
+        }
+        
+        vzsJogadas++;
+        if(vzsJogadas >= 9){
+            printf("DEU VELHA !");
+        }
+    }while (vzsJogadas < 9);
 
     return 0;
 }
