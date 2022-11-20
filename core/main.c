53f57b7fadc9fbf0f8e4456402fc601cc2180987
Pedro Neves <contato.nevespedro@gmail.com>
2022-11-20 01:32:02 -0300

    lipado a main e inserido switch case do menu

--- core/main.c
+++ core/main.c
@@ -15,57 +15,16 @@
 
 int main(){
 
-    char **matriz;
-    int vezJogador = 1;
-    int flagDeErros;
-    int vencedor =0;
-    int vzsJogadas = 0;
-    int linha, coluna;
-    int *row, *col;
-
-    matriz = malloc(3 * sizeof(char*));
-    for (int i = 0; i < 3; i++)
+    switch (menu())
     {
-        matriz[i] = malloc(3 * sizeof(char));
-    }
+    case 1:
+        tictactoe();
+        break;
     
-    preencherMatriz(matriz);
-    mostrarMatriz(matriz);
-
-    row = &linha;
-    col = &coluna;
-
-    do{
-        printf("\nvez do jogador %d\n", vezJogador);
-        do{
-            pegarPonto(row, col);
-            
-            flagDeErros = verificarPonto(row, col, matriz);
-            if(flagDeErros > 0){
-                printf("coordenada invalida, digite novamente \n");
-            }
-        }while(flagDeErros != 0);
-
-        mudarMatriz(row, col, matriz, vezJogador);
-        system("cls");
-        mostrarMatriz(matriz);
-        if(vezJogador == 1){
-            vezJogador = 2;
-        }else{
-            vezJogador = 1;
-        }
-
-        vencedor = verificarVencedor(matriz);
-        if(vencedor != 0){
-            printf("\n O JOGADOR %d E O VENCEDOR !!! \n", vencedor);
-            break;
-        }
-        
-        vzsJogadas++;
-        if(vzsJogadas >= 9){
-            printf("DEU VELHA !");
-        }
-    }while (vzsJogadas < 9);
+    default:
+        printf("caiu no default");
+        break;
+    }
 
     return 0;
 }
